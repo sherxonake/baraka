bc9cddd5f328a8d259e0c9a76e15142787961d38
sherxonake <134454572+sherxonake@users.noreply.github.com>
2026-02-02 15:08:56 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+// Плавный скролл к якорям
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
